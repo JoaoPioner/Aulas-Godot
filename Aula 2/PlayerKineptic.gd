caf957b81234b839809b4e9da6475a2882797604
JoaoPioner <jjvpioner@gmail.com>
2022-09-22 08:51:43 -0300

    camera adicionada

--- Aula 2/PlayerKineptic.gd
+++ Aula 2/PlayerKineptic.gd
@@ -2,6 +2,8 @@ extends KinematicBody2D
 
 export (int) var speed = 200
 export (float) var rotation_speed = 1.5
+export (int) var jump_speed = 1000
+export (int) var gravity = 2500
 
 var velocity = Vector2()
 var rotation_dir = 0
@@ -44,6 +46,21 @@ func get_mouse_input():
 		velocity = Vector2(-speed, 0).rotated(rotation)
 	if Input.is_action_pressed("ui_up"):
 		velocity = Vector2(speed, 0).rotated(rotation)
+		
+func get_side_input():
+	velocity.x = Input.get_action_strength("ui_right")-Input.get_action_strength("ui_left")
+	velocity.x *= speed
+	if(velocity.x>0):
+		sprite.play("Right")
+	elif(velocity.x<0):
+		sprite.play("Left")
+	elif(velocity.y>0):
+		sprite.play("Down")
+	else:
+		sprite.stop()
+		sprite.frame = 0
+	if is_on_floor() and Input.is_action_just_pressed('ui_up'):
+		velocity.y = -jump_speed
 
 func _input(event):
 	if event.is_action_pressed("click"):
@@ -52,7 +69,7 @@ func _input(event):
 
 func _physics_process(delta):
 	#Movimentacao Direcional
-	get_8way_input()
+	#get_8way_input()
 	
 	#Movimentacao Rotacional( tipo carro)
 	#get_rotation_input()
@@ -62,7 +79,13 @@ func _physics_process(delta):
 	#get_mouse_input()
 	
 	#Deslocamento
-	velocity = move_and_slide(velocity)
+	#velocity = move_and_slide(velocity)
+	
+	#side scroller
+	get_side_input()
+	velocity.y += gravity*delta
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
 	
 	#Movimentacao mouse 2 (vai onde vc clica)
 	#velocity = position.direction_to(target) * speed
